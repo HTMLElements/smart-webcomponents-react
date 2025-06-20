86c4d1e25857eca053f31dc70351bdb2db6e2e59
bmarkov <boikom@jqwidgets.com>
2025-06-20 19:21:24 +0300

    up

---
+++ demos/kanban/overview/src/App.jsx
@@ -0,0 +1,39 @@
+import 'smart-webcomponents-react/source/styles/smart.default.css';
+import './App.css';
+import { Kanban } from 'smart-webcomponents-react/kanban';
+import { GetKanbanData } from './common/data';
+
+function App() {
+	const dataSource = GetKanbanData();
+	const columns = [{
+		label: 'To do',
+		dataField: 'toDo'
+	},
+	{
+		label: 'In progress',
+		dataField: 'inProgress'
+	},
+	{
+		label: 'Testing',
+		dataField: 'testing'
+	},
+	{
+		label: 'Done',
+		dataField: 'done'
+	}
+	]
+
+	return (
+		<div>
+			<Kanban
+				columns={columns}
+				dataSource={dataSource}
+				collapsible>
+			</Kanban>
+		</div>
+	);
+}
+
+
+
+export default App;
